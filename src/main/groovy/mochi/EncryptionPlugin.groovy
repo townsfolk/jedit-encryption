dc341be1f72de5cede1daa73555eb858e8b229b8
townsfolk <elberry@gmail.com>
2016-12-05 20:37:10 -0800

    Deleting decrypted file when file was previously a encrypted file

--- src/main/groovy/mochi/EncryptionPlugin.groovy
+++ src/main/groovy/mochi/EncryptionPlugin.groovy
@@ -16,6 +16,7 @@ public class EncryptionPlugin extends EBPlugin {
 	public static final String JASYPT = "jasypt"
 	public static final EncryptionPlugin instance = new EncryptionPlugin()
 	public static final String ENCRYPTED_EXTENSION = ".jenc"
+	private Set<String> sessionFiles = []
 
 	public File decryptFile(String path) {
 		Log.log(Log.DEBUG, this, "Decrypting file: $path")
@@ -63,19 +64,48 @@ public class EncryptionPlugin extends EBPlugin {
 		if (message instanceof BufferUpdate) {
 			BufferUpdate updateMessage = (BufferUpdate) message
 			Buffer buffer = updateMessage.buffer
-			if (BufferUpdate.CLOSED == updateMessage.getWhat()) {
-				// TODO: if (buffer was decrypted buffer) then delete buffer's file on file system
+			// if (buffer was decrypted buffer) then decrypt current content, and delete decrypted file on file system
+			if (BufferUpdate.CLOSED == updateMessage.getWhat() && sessionFiles.contains(buffer.path)) {
+				cleanupDecryptedFile(buffer)
 			}
+			// if (buffer is encrypted) then close buffer, decrypt content to decrypted file, and open decrypted path
 			if (BufferUpdate.LOADED == updateMessage.getWhat() && isEncrypted(updateMessage.buffer.path)) {
-				File decryptedFile = decryptFile(buffer.path)
-				jEdit.closeBuffer(updateMessage.view, buffer)
-				jEdit.openFile(updateMessage.view, decryptedFile.path)
-				// TODO: if (buffer.path is encrypted) then: close buffer, create new decrypted file, decrypt content, save to decrypted file, finally open new buffer with decrypted path
+				decryptAndOpen(updateMessage)
 			}
 		}
 		// listen for buffer.loaded - check if encrypted, then decrypt to new file
 		// listen for buffer.saving - may not be needed
 		// listen for buffer.closed - if file was decrypted, then encrypt contents, and delete decrypted file
+	}
+
+	private void cleanupDecryptedFile(Buffer buffer) {
+		String path = buffer.path
+		if (encryptFile(path)) {
+			try {
+				if (!new File(path).delete()) {
+					Log.log(Log.ERROR, this, "Unable to delete decrypted file: ${path}")
+				} else {
+					sessionFiles.remove(path)
+				}
+			} catch (Exception e) {
+				Log.log(Log.ERROR, this, "Unable to delete decrypted file: ${path}", e)
+			}
+		} else {
+			Log.log(Log.ERROR, this, "Unable to encrypt file: ${path}")
+			// todo: alert user about error, reopen decrypted file?
+		}
+	}
+
+	private void decryptAndOpen(BufferUpdate updateMessage) {
+		Buffer buffer = updateMessage.buffer
+		File decryptedFile = decryptFile(buffer.path)
+		if (decryptedFile) {
+			jEdit.closeBuffer(updateMessage.view, buffer)
+			jEdit.openFile(updateMessage.view, decryptedFile.path)
+			sessionFiles << decryptedFile.path
+		} else {
+			Log.log(Log.ERROR, this, "Unable to decrypt file: ${buffer.path}")
+		}
 	}
 
 	public static EncryptionPluginOptions optionPane() {
